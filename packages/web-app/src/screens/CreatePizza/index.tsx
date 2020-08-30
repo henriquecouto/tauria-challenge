7a06dbc733e5ecc90112d4741531a2e7c889b293
Henrique Couto <henrique.coutu@live.com>
2020-08-30 13:06:50 -0300

    create pizza form with state

--- packages/web-app/src/screens/CreatePizza/index.tsx
+++ packages/web-app/src/screens/CreatePizza/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Container,
   Form,
@@ -14,15 +14,41 @@ import { usePizzas } from "../../contexts/pizza";
 const CreatePizza: React.FC = () => {
   const { options } = usePizzas();
 
+  const [name, setName] = useState("");
+  const [size, setSize] = useState("");
+  const [crust, setCrust] = useState("");
+  const [toppings, setToppings] = useState(Array<string>());
+
+  console.log({ name, size, crust, toppings });
+
+  const onChangeToppings = ({
+    target: { name, checked },
+  }: React.ChangeEvent<HTMLInputElement>) => {
+    if (checked) {
+      setToppings((old) => [...old, name]);
+    } else {
+      setToppings((old) => old.filter((topping) => topping !== name));
+    }
+  };
+
   return (
     <Container>
       <Form>
         <Label>
-          Name: <Input placeholder="Insert a name for your pizza!" />
+          Name:{" "}
+          <Input
+            placeholder="Insert a name for your pizza!"
+            value={name}
+            onChange={(event) => setName(event.target.value)}
+          />
         </Label>
         <Label>
           Size:
-          <Select>
+          <Select
+            value={size}
+            onChange={(event) => setSize(event.target.value)}
+          >
+            <option value="">select</option>
             {Object.keys(options.pizzaSizesPrice).map((size) => (
               <option key={size}>{size}</option>
             ))}
@@ -30,7 +56,11 @@ const CreatePizza: React.FC = () => {
         </Label>
         <Label>
           Crust:
-          <Select>
+          <Select
+            value={crust}
+            onChange={(event) => setCrust(event.target.value)}
+          >
+            <option value="">select</option>
             {Object.keys(options.pizzaCrustTypePrice).map((size) => (
               <option key={size}>{size}</option>
             ))}
@@ -41,7 +71,11 @@ const CreatePizza: React.FC = () => {
           <CheckboxItens>
             {Object.keys(options.pizzaToppings).map((topping) => (
               <CheckboxLabel key={topping}>
-                <input type="checkbox" />
+                <input
+                  type="checkbox"
+                  name={topping}
+                  onChange={onChangeToppings}
+                />
                 <span>{topping}</span>
               </CheckboxLabel>
             ))}
